420545facb4a765d9baf0ccbae94827ef76ae6ef
CodyKide <1551656637@qq.com>
2025-01-24 17:55:14 +0800

    Site updated: 2025-01-24 17:55:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/03/26/hello-world/","2023/03/26/README/"];function toRandomPost(){
+var posts=["2023/03/26/README/","2023/03/26/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
